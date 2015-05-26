84cd7c01f1cd0dd3c1b6e9a698ea22ba7ca989a6
Dzmitry Misiuk <minsler.by@gmail.com>
2015-05-27 01:13:43 +0300

    Added post method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 extern crate hyper;
 extern crate url;
-extern crate rustc_serialize;
-
-use rustc_serialize::json::{self, Json};
+extern crate serde;
 
 use hyper::client::Client;
-use hyper::header::{ContentType, Connection};
+use hyper::header::ContentType;
 use hyper::error::Error;
 use std::io::Read;
 use url::Url;
 use hyper::status::StatusCode;
-
+use serde::json::{self,Value};
 
 pub struct HttpResponse {
     pub status: StatusCode,
@@ -18,33 +16,73 @@ pub struct HttpResponse {
 }
 
 fn main() {
-    let url: Url = Url::parse("http://localho3st:8983/solr/gettingstarted/select?q=foundation&wt=json&indent=true").unwrap();
+    let url: Url = Url::parse("http://localhost:8983/solr/gettingstarted/select?q=*:*&wt=json&indent=true").unwrap();
 
+    let url_post = Url::parse("http://localhost:8983/solr/gettingstarted/update").unwrap();
     let r = get(&url);
     match r {
-    	Ok(response) => {
-    		println!("{}", response.body)
-    	},
-    	Err(e) => println!("Some error occured: {:?}", e)
+        Ok(response) => {
+            //println!("{}", response.body);
+            let json_resp: Value= json::from_str(&response.body).unwrap();
+            let docs = json_resp.as_object().unwrap().get("response").unwrap().as_object().unwrap().get("docs").unwrap();
+            for doc in docs.as_array().unwrap() {
+                println!("{:?}", doc);
+            }
+
+        },
+        Err(e) => println!("Some error occured: {:?}", e)
+    }
+
+    let r_post = post(&url_post);
+
+    match r_post {
+        Ok(response) => {
+            println!("{}", response.body)
+        },
+        Err(e) => println!("Some error occured: {:?}", e)
     }
 }
 
 pub fn get(url: &Url) -> Result<HttpResponse, Error> {
-	let mut client = Client::new();
-   	let mut result_response = client.get(&url.to_string()).send();
-	match result_response {
-		Ok(mut res) => {
-			let mut body = String::new();
-		    let result = res.read_to_string(&mut body);
-		    match result {
-		        Ok(_) => {
-		            Ok(HttpResponse{status: res.status, body: body})
-		        },
-		        Err(err) => {
-		            Err(Error::Io(err))
-		        }
-		    }
-		},
-		Err(err) => Err(err)
-	}		
+    let mut client = Client::new();
+    let result_response = client.get(&url.to_string()).send();
+    match result_response {
+        Ok(mut res) => {
+            let mut body = String::new();
+            let result = res.read_to_string(&mut body);
+            match result {
+                Ok(_) => {
+                    Ok(HttpResponse{status: res.status, body: body})
+                },
+                Err(err) => {
+                    Err(Error::Io(err))
+                }
+            }
+        },
+        Err(err) => Err(err)
+    }
+}
+
+
+pub fn post(url: &Url) -> Result<HttpResponse, Error> {
+    let mut client = Client::new();
+    let result_response = client.post(&url.to_string())
+        .header(ContentType::json())
+        .body("{add: {doc: {id: \"7\", title: \"petia2\", v: {inc: 1}}, boost: 1, overwrite: true, commitWithin: 1000}}")
+        .send();
+    match result_response {
+        Ok(mut res) => {
+            let mut body = String::new();
+            let result = res.read_to_string(&mut body);
+            match result {
+                Ok(_) => {
+                    Ok(HttpResponse{status: res.status, body: body})
+                },
+                Err(err) => {
+                    Err(Error::Io(err))
+                }
+            }
+        },
+        Err(err) => Err(err)
+    }
 }
